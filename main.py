a02dcd52f2a9da23935373c6c2f362930af6f0b6
pinnertw <wayne567tw@yahoo.com.tw>
2021-02-11 17:02:17 +0100

    [DEV] Multimeasure starts with -1.5, single simulation test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     from src.test import multimeasure
-    #multimeasure.histogram_test(-4, 1, int(1e6))
-    #multimeasure.limite_law(1, 1.5, int(1e4), int(1e3))
-    multimeasure.potential()
+    multimeasure.histogram_test(-4, 0.1, int(1e6))
+    #multimeasure.limite_law(-1.5, 0.99, int(1e4), int(1e3))
+    #multimeasure.potential()
     #OU.single_curve([0, 1, 2])
     #OU.heatmap([1, 2])
     #OU.tcl(M=50)
